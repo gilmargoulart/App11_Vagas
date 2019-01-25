bc1262fe1f800b29c40cce9413bfd7450aa1670e
Gilmar J. A. Goulart <gilmar.g@outlook.com>
2019-01-24 22:04:06 -0200

    Commit inicial

---
+++ App11_Vagas/App11_Vagas/Modelos/Vaga.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App11_Vagas.Modelos
+{
+    public class Vaga
+    {
+        public string NomeVaga { get; set; }
+        public short Quantidade { get; set; }
+        public string Cidade { get; set; }
+        public double Salario { get; set; }
+        public string Descricao { get; set; }
+        public string TipoContratacao { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
